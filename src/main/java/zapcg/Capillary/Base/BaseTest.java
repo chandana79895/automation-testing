fc9e27b0d5f7280e88968468ab4d41c0d59dc1a3
bharath.kumar <bharath.kumar@capillarytech.com>
2024-07-01 14:52:11 +0530

    test file updated

--- src/main/java/zapcg/Capillary/Base/BaseTest.java
+++ src/main/java/zapcg/Capillary/Base/BaseTest.java
@@ -33,10 +33,10 @@ public class BaseTest {
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--remote-allow-origins=*");
-                // options.addArguments("--headless");
-                // options.addArguments("--no-sandbox");
+                options.addArguments("--headless");
+                options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
-                // options.addArguments("--disable-gpu");
+                options.addArguments("--disable-gpu");
                 driver = new ChromeDriver(options);
                 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
 
