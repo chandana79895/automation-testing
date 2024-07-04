e1b75acb909e67fda2826da76eb4efbbcf2dd0b8
Chandana2510 <chandanagovada12@gmail.com>
2024-07-04 17:43:27 +0530

    version removed

--- src/main/java/zapcg/Capillary/Base/BaseTest.java
+++ src/main/java/zapcg/Capillary/Base/BaseTest.java
@@ -30,7 +30,7 @@ public class BaseTest {
     public void initialization(String browser) {
     try {
         if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().driverVersion("126.0.6478.57").setup();
+            WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--remote-allow-origins=*");
             options.addArguments("--headless");
