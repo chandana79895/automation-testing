6e2098d7b6925898e7692136b92f86dafa6f53aa
Chandana2510 <chandanagovada12@gmail.com>
2024-07-09 14:28:00 +0530

    added code

---
+++ src/test/java/zapcg/Capillary/LoginTestCases/Login_Valid_TestCases.java
@@ -0,0 +1,55 @@
+package zapcg.Capillary.LoginTestCases;
+
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import zapcg.Capillary.Base.BaseTest;
+import zapcg.Capillary.PageObject.loginPage;
+
+
+
+@Listeners(zapcg.Capillary.ListenersConfiguration.Listeners.class)			
+
+public class Login_Valid_TestCases extends BaseTest {
+	
+	 public loginPage lp;
+	 public String currentBrowser;
+	   
+	
+	 @BeforeMethod
+		@Parameters({"browser", "deviceName"})
+	   public void initialize(String browser, String deviceName) throws InterruptedException {
+		// logger.info("Initializing test method");
+		 setUp(browser, deviceName); // Use the setup method to initialize the browser
+	       initialization(browser);
+	       driver.get(baseUrl);
+	       Thread.sleep(1000); // For demonstration purposes, avoid using Thread.sleep in real tests
+	       lp = new loginPage(driver);
+	   
+	       lp.changeDefaultLanguage();
+	       lp.chooseEnglishLanguage();
+	       
+
+	   }
+	
+	
+
+	@Test(priority = 1)
+    public void testValidLogin_TestCase1() throws InterruptedException {
+	        lp.login("zapcom_test2", "storeportal");
+	      
+	        lp.verifySuccessfullNavigationFromLogin(driver);
+	        driver.close();
+		
+        
+		}
+	
+	
+	
+}
+	
+	
+
