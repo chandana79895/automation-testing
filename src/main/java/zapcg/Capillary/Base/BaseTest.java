e80db20f2894701fcfc79eb229b631023bc0d196
Chandana2510 <chandanagovada12@gmail.com>
2024-07-11 17:54:00 +0530

    test of check

--- src/main/java/zapcg/Capillary/Base/BaseTest.java
+++ src/main/java/zapcg/Capillary/Base/BaseTest.java
@@ -33,8 +33,9 @@ public class BaseTest {
         try {
             if (browser.equalsIgnoreCase("chrome")) {
                 // Hardcoding Chrome driver path
-                System.setProperty("webdriver.chrome.driver", "/home/runner/actions-runner/_work/automation-testing/automation-testing/src/resources/chromedriver-linux64/chromedriver");
+                // System.setProperty("webdriver.chrome.driver", "/home/runner/actions-runner/_work/automation-testing/automation-testing/src/resources/chromedriver-linux64/chromedriver");
                 // WebDriver driver = new ChromeDriver();
+                WebDriverManager.chromedriver().setup();
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--remote-allow-origins=*");
                 options.addArguments("--headless"); // Enable headless mode
